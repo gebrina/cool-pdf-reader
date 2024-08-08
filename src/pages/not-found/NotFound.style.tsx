6a4d4e7ae19ef330bd35b51737a758d7a8b87a1f
gebrina <gsonofmika@gmail.com>
2024-08-08 15:17:41 +0300

    style not found page

--- src/pages/not-found/NotFound.style.tsx
+++ src/pages/not-found/NotFound.style.tsx
@@ -6,14 +6,42 @@ import { selectColors } from "../../utils";
 type TPageNotFoundProps = {
   theme: ThemeName;
 };
+
 export const PageNotFound = styled.section<TPageNotFoundProps>(
   ({ theme }) => css`
     transform: scaleX(-1);
     color: ${selectColors(theme).titleColor};
-    font-size: 1.5rem;
+    font-size: 2.5rem;
     display: flex;
-    gap: 0.3rem;
     align-items: center;
     justify-content: center;
+    gap: 1rem;
+    flex-direction: column;
+    svg {
+      font-size: 7rem;
+      color: #d9a4d0;
+    }
+    button {
+      font-size: 1.5rem;
+      border: none;
+      background-color: transparent;
+      cursor: pointer;
+      -webkit-text-stroke-width: 2px;
+      padding: 0.5rem 1rem;
+      color: ${selectColors(theme).titleColor};
+      box-shadow: 3px 3px 0.3px 3px rgba(255, 0, 0, 0.5),
+        -1px -1px 0.3px 3px rgba(148, 224, 247, 0.5);
+      border-radius: 0.5rem;
+      &:hover {
+        background-color: ${selectColors(theme).bgColor};
+        color: ${selectColors(theme).textColor};
+        transition: all 0.5s ease-in-out;
+        box-shadow: -1px -1px 0.3px 3px rgba(148, 224, 247, 0.5),
+          3px 3px 0.3px 3px rgba(255, 0, 0, 0.5);
+        border-radius: 0.5rem;
+      }
+    }
   `
 );
+
+export const Button = styled.button``;
--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,12 +1,20 @@
 import { BiXCircle } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 import { usePdfContext } from "../../context";
-import { PageNotFound } from "./NotFound.style";
+import { Button, PageNotFound } from "./NotFound.style";
 
 const NotFound = () => {
   const { theme } = usePdfContext();
+  const navigate = useNavigate();
+
+  const handleBackToHomeClick = () => navigate("/");
+
   return (
     <PageNotFound theme={theme}>
       <BiXCircle /> <h1>Page Not Found</h1>
+      <Button onClick={handleBackToHomeClick} role="link">
+        Back to Home
+      </Button>
     </PageNotFound>
   );
 };
