7dded66ece56e9a84c914e61bfd157490b4e59ef
gebrina <gsonofmika@gmail.com>
2024-08-10 16:47:44 +0300

    style pdf viewer using after selector

--- src/pages/viewer/Viewer.style.tsx
+++ src/pages/viewer/Viewer.style.tsx
@@ -1,6 +1,7 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { ThemeName } from "../../common";
+import { selectColors } from "../../utils";
 
 type PdfViewerProps = {
   theme: ThemeName;
@@ -9,11 +10,26 @@ type PdfViewerProps = {
 export const PdfViewer = styled.div<PdfViewerProps>(
   ({ theme }) => css`
     transform: scaleX(-1);
+    height: 100%;
+    width: 100%;
+    max-height: 100vh;
+    overflow: hidden;
     * {
       color: #d5b8b8;
       font-size: 1rem;
       font-weight: 600;
       text-align: center;
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      mix-blend-mode: multiply;
+      top: 0;
+      width: inherit;
+      height: inherit;
+      background-color: ${selectColors(theme).bgColor};
+      z-index: 1;
     }
   `
 );
