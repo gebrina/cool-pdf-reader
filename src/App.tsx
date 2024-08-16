660e32d45511a070bc85a0f70138d7c96ae9fd15
gebrina <gsonofmika@gmail.com>
2024-08-16 22:04:01 +0300

    create setting page for user preferences

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import beautyGirl from "./assets/bg.jpeg";
 import { usePdfContext } from "./context";
 import { PdfUploader, PdfViewer } from "./pages";
 import NotFound from "./pages/not-found/NotFound";
+import { Setting } from "./pages/setting";
 
 export const App = () => {
   const { theme } = usePdfContext();
@@ -16,6 +17,7 @@ export const App = () => {
       <Routes>
         <Route path="" element={<PdfUploader />} />
         <Route path="/:filename" element={<PdfViewer />} />
+        <Route path="settings" element={<Setting />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </PdfContainer>
